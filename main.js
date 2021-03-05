0a2c86ce55e1354adbffa9018eb36e4205f70acf
Miguel Angel Carvajal Gonzalez <36669778+amcarvajal@users.noreply.github.com>
2021-03-05 12:13:26 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function singlePost(){
+document.getElementById("contenedor").style.display="none";
+document.getElementById("singlePost").style.display="block";
+}
+function multiPost(){
+    document.getElementById("contenedor").style.display="block";
+    document.getElementById("singlePost").style.display="none";
+}
